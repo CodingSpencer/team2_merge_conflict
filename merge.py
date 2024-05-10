c2db587cc15a5dd5349c91e1208f1484ed99ba84
LukeWellsey <lukewells768@gmail.com>
2024-05-10 12:16:18 -0600

    added duck_logic

--- merge.py
+++ merge.py
@@ -2,8 +2,13 @@ def print_stuff():
     stuff = "This is some info"
     print(stuff)
 
+def duck_logic():
+    quack = input('Whats the difference between a duck?')
+    print(f'{quack} is indeed the difference between a duck.')
+
 def main():
     print_stuff()
+    duck_logic()
 
 if __name__ == "__main__":
     main()
