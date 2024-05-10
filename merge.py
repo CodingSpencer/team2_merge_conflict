c72f31a081bc0ddf924a8088fb8942aac3b900d8
Nathan <bro19053@byui.edu>
2024-05-10 12:10:49 -0600

    Added nathans function

--- merge.py
+++ merge.py
@@ -7,8 +7,12 @@ def print_stuff():
     stuff = "Hi ", get_stuff()
     print(stuff)
 
+def nathans_function():
+    print("Nathan's function")
+
 def main():
     print_stuff()
+    nathans_function()
 
 if __name__ == "__main__":
     main()
